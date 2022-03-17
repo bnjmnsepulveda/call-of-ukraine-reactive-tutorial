7311c83ad699511e1fbafcb4c0641e515927f55a
Benjamin <benjamin@Benjamins-MacBook-Pro.local>
2022-03-17 19:50:03 -0300

    routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,37 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { RankingModule } from './presentation/pages/ranking/ranking.module';
-import { SoldierRankingComponent } from './presentation/pages/ranking/soldier-ranking/soldier-ranking.component';
 import { RegisterModule } from './presentation/pages/register/register.module';
+import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
+import { NewSoldierComponent } from './presentation/pages/register/new-soldier/new-soldier.component';
+
+const routes: Routes = [
+  { 
+    path: 'register', 
+    component: NewSoldierComponent
+  },
+  {
+    path: '', 
+    redirectTo: '/register', 
+    pathMatch: 'full' 
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     RegisterModule,
     RankingModule,
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
